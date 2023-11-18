318de2e26e1dc17532848f33b3b067be4631acb0
Bhattarai420 <05220168.jnec@rub.edu.bt>
2023-11-18 13:34:45 +0600

    File Uploaded

---
+++ script.js
@@ -0,0 +1,7 @@
+function validateForm(){
+    const sidRegex = /^[0-9]{7}$/; //regex to check student id
+    const nameRegex = /^[a-zA-Z0-9_-]{3,20}$/; //regex to check student name
+
+    // your code goes here
+}
+
